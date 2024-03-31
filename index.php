f44961087918fe34a1a36be416edcaf83b2f13bc
Ragab Mohamed Ammar <39869369+Yass-Gfx@users.noreply.github.com>
2024-03-31 04:47:07 +0200

    Update index.php
    when Try to fitch the row in posts it was creating columns above each other and what i did to close the row after the 3 columns to make it right

--- index.php
+++ index.php
@@ -15,45 +15,49 @@ get_header();
 			if ( have_posts() ) :
 				?>
 				<div class="container">
-					<?php
-					if ( is_home() && ! is_front_page() ) {
-						?>
-						<header class="mb-5">
-							<h1 class="page-title screen-reader-text">
-								<?php single_post_title(); ?>
-							</h1>
-						</header>
-						<?php
-					}
-					?>
 
-					<div class="row">
-						<?php
-						$index         = 0;
-						$no_of_columns = 3;
+                <?php
+                if (is_home()) {
+                ?>
+                    <header class="mb-5">
+                        <h1 class="page-title">
+                            <?php single_post_title(); ?>
+                        </h1>
+                    </header>
+                <?php
+                }
+                ?>
 
-						while ( have_posts() ) : the_post();
+                <?php
+                $index         = 0;
+                $no_of_columns = 3;
 
-							if ( 0 === $index % $no_of_columns ) {
-								?>
-								<div class="col-lg-4 col-md-6 col-sm-12">
-								<?php
-							}
+                while (have_posts()) : the_post();
 
-							get_template_part( 'template-parts/content' );
+                    if (0 === $index % $no_of_columns) {
+                ?>
+                        <div class="row">
+                        <?php
+                    }
+                        ?>
+                        <div class="col-lg-4 col-md-6 col-sm-12">
+                            <?php
+                            get_template_part('template-parts/content');
+                            ?>
+                        </div>
+                        <?php
+                        $index++;
 
-							$index ++;
+                        if (0 !== $index && 0 === $index % $no_of_columns) {
+                        ?>
+                        </div>
+                <?php
+                        }
 
-							if ( 0 !== $index && 0 === $index % $no_of_columns ) {
-								?>
-								</div>
-								<?php
-							}
+                    endwhile;
+                ?>
 
-						endwhile;
-						?>
-					</div>
-				</div>
+            </div>
 			<?php
 
 			else :
